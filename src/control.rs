53378d79a6d514ac172a49e8c02f74f747be686e
Sherlock Holo <sherlockya@gmail.com>
2021-12-11 23:18:35 +0800

    refactor: remove useless run_controller

--- src/control.rs
+++ src/control.rs
@@ -1,49 +1,16 @@
 use anyhow::Result;
-use futures_util::{StreamExt, TryStreamExt};
-use kube::api::ListParams;
+use futures_util::TryStreamExt;
 use kube::runtime::controller::Context;
 use kube::runtime::reflector::store::Writer;
 use kube::runtime::reflector::ObjectRef;
-use kube::runtime::Controller;
-use kube::{Api, Client};
-use tracing::{error, info};
+use kube::Client;
+use tracing::info;
 
 use crate::cf_dns::CfDns;
 use crate::reconcile;
 use crate::reconcile::reflect::reflect;
 use crate::reconcile::schedule::schedule;
 use crate::reconcile::ContextData;
-use crate::spec::*;
-
-pub async fn run_controller() -> Result<()> {
-    let client = Client::try_default().await?;
-
-    info!("init k8s client");
-
-    let cf_dns = CfDns::new().await?;
-
-    info!("init cf dns client");
-
-    let ctx = Context::new(ContextData {
-        client: client.clone(),
-        cf_dns,
-    });
-
-    let ddns_api: Api<Ddns> = Api::all(client);
-
-    Controller::new(ddns_api, ListParams::default())
-        .run(reconcile::reconcile, reconcile::reconcile_failed, ctx)
-        .for_each(|res| async move {
-            if let Err(err) = res {
-                error!(%err, "reconcile failed");
-            }
-        })
-        .await;
-
-    error!("controller stop");
-
-    Err(anyhow::anyhow!("controller stop"))
-}
 
 pub async fn run_scheduler() -> Result<()> {
     let client = Client::try_default().await?;
